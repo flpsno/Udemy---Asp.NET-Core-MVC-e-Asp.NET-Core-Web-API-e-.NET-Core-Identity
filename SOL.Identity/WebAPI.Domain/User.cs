cdf8873d358a305c5fd3cd9ac933b36be27c3425
Felipe Santos <flpsno@hotmail.com>
2020-04-02 11:07:14 -0300

    adicionando o projeto WebAPI.Repository.

---
+++ SOL.Identity/WebAPI.Domain/User.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Domain
+{
+    public class User : IdentityUser<int>
+    {
+        public string NomeCompleto { get; set; }
+        public string Member { get; set; } = "Member";
+        public string OrgId { get; set; }
+
+        public List<UserRole> UserRoles { get; set; }
+
+    }
+}
